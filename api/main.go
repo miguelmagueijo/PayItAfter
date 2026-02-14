3ab30ade94cc74152b121e3f877d53cbd2c8a943
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-02-14 16:18:31 +0000

    api: init gin

---
+++ api/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+
+	err := router.Run()
+
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+}
