c3271d8c9dd12502f43d0fdf413f8df8e02523ad
Miguel Magueijo <miguelmagueijo@hotmail.com>
2026-03-03 22:12:46 +0000

    app: add .env file

--- app/constants/helpers/api.ts
+++ app/constants/helpers/api.ts
@@ -1,4 +1,4 @@
-export const API_URL = "http://192.168.1.126:8900";
+export const API_URL = process.env.EXPO_PUBLIC_API_URL;
 
 export const getAuthHeader = (token: string) => {
 	return `PIA ${token}`;
